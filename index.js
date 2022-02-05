6ae99ad4d45cd9193498c6bc864f3aaf6144bfdb
mckaymatt3 <mckaymatt@outlook.com>
2022-02-05 18:46:59 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+function reducer (array) {
+    return array.reduce(function checkTotal (previousValue,currentValue) {
+        return currentValue + previousValue
+    },0) 
+} 
+
+const totalBatteries = reducer(batteryBatches);
